3df7abb3b2a924eeb14d8c65d90f085dfa733cef
Darya Lapitskaya <d.kov0809@gmail.com>
2021-09-20 13:34:35 +0300

    Commit2

--- main.js
+++ main.js
@@ -19,5 +19,5 @@ fs.moveSync(dir1+'/'+fileName, dir2+'/'+fileName) //move file from the first fol
 fs.emptyDirSync(dir2) //delete the file by deletion of the content of the second folder
 fs.removeSync(dir1) //delete the first folder
 fs.removeSync(dir2) //delete the second folder
-console.log('from Main');
+console.log('Branch2');
 
